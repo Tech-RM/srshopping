d15f68a181d9c5763dad56a8824c758c22d8ab34
Tech-RM <techramesh888@gmail.com>
2023-06-11 11:22:39 +0530

    added everything except payment gateway

--- src/components/directory-item/directory-item.component.js
+++ src/components/directory-item/directory-item.component.js
@@ -1,14 +1,16 @@
+import { Link } from 'react-router-dom';
 import './directory-item.styles.scss';
 
 const DirectoryItem=({category})=>{
     
+  const {title}= category;
     return(
         <div className="directory-item-container">
           <div className="background-image" style={{backgroundImage:`url(${category.imageUrl})`}}/>
-          <div className="directory-body-container">
-            <h2>{category.title}</h2>
+          <Link to={`shop/${title.toLowerCase()}`} className="directory-body-container">
+            <h2>{title}</h2>
             <p>Shop Now!</p>
-          </div>
+          </Link>
       </div>
     )
 }
